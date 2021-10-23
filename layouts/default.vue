fc04cfefea191164cb278d1001be7cf728efb9a6
laloinsane <calisto.michel@gmail.com>
2021-10-23 20:51:13 -0300

    update footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,13 +44,7 @@
         <div class="row">
           <div class="col l6 s12">
             <h5 class="footer-title">Store Bio</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-              interdum congue ante, eu ultrices eros finibus ut. Donec libero
-              ligula, varius tincidunt metus non, aliquet faucibus purus.
-              Vivamus eleifend ligula metus, eget cursus quam iaculis vel. Donec
-              enim lacus, scelerisque nec consequat eu, dapibus in augue.
-            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum congue ante, eu ultrices eros finibus ut. Donec libero ligula, varius tincidunt metus non, aliquet faucibus purus. Vivamus eleifend ligula metus, eget cursus quam iaculis vel. Donec enim lacus, scelerisque nec consequat eu, dapibus in augue.</p>
           </div>
           <div class="col l3 s12">
             <h5 class="footer-title">Site Links</h5>
@@ -64,22 +58,14 @@
       <div class="footer-copyright">
         <div class="container desktop">
           <div class="row">
-            <div class="col s12">
-              © Copyright <a class="footer-link" href="#">Grúas Behrend</a>. Todos los derechos reservados. Powered by <a class="footer-link" href="#">Dev-Manage</a>.
-            </div>
+            <div class="col s12">© Copyright <a class="footer-link" href="#">Grúas Behrend</a>. Todos los derechos reservados. Powered by <a class="footer-link" href="#">Dev-Manage</a>.</div>
           </div>
         </div>
         <div class="container mobile center">
           <div class="row">
-            <div class="col s12">
-              © Copyright <a class="footer-link" href="#">Grúas Behrend</a>.
-            </div>
-            <div class="col s12">
-              Todos los derechos reservados.
-            </div>
-            <div class="col s12">
-              Powered by <a class="footer-link" href="#">Dev-Manage</a>.
-            </div>
+            <div class="col s12">© Copyright <a class="footer-link" href="#">Grúas Behrend</a>.</div>
+            <div class="col s12">Todos los derechos reservados.</div>
+            <div class="col s12">Powered by <a class="footer-link" href="#">Dev-Manage</a>.</div>
           </div>
         </div>
       </div>
