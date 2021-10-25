0869ad7bb795e6d3dfdcffa793a1baaebe06e37c
laloinsane <calisto.michel@gmail.com>
2021-10-25 16:47:51 -0300

    update servicios

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <div class="section center">
       <div class="row">
+        <h3>Nosotros</h3>
         <div class="column s12">
-          <h3>Nosotros</h3>
-          <h5>Somos una empresa líder en la carga, arrastre, enganche de vehículos en Osorno y hacia el exterior.</h5>
+          <h5 class="thin">Somos una empresa líder en la carga, arrastre, enganche de vehículos en Osorno y hacia el exterior.</h5>
         </div>
       </div>
     </div>
@@ -13,24 +13,24 @@
       <div class="row">
         <h3>Servicios</h3>
         <div class="col s12 m4">
-          <div class="center promo promo-example">
+          <div class="center">
             <i class="large material-icons">flash_on</i>
-            <p class="promo-caption">Speeds up development</p>
-            <p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+            <p class="bold">Speeds up development</p>
+            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
           </div>
         </div>
         <div class="col s12 m4">
-          <div class="center promo promo-example">
+          <div class="center">
             <i class="large material-icons">group</i>
-            <p class="promo-caption">User Experience Focused</p>
-            <p class="light center">By utilizing elements and principles of Material Design, we were able to create a framework that focuses on User Experience.</p>
+            <p class="bold">User Experience Focused</p>
+            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that focuses on User Experience.</p>
           </div>
         </div>
         <div class="col s12 m4">
-          <div class="center promo promo-example">
+          <div class="center">
             <i class="large material-icons">settings</i>
-            <p class="promo-caption">Easy to work with</p>
-            <p class="light center">We have provided detailed documentation as well as specific code examples to help new users get started.</p>
+            <p class="bold">Easy to work with</p>
+            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started.</p>
           </div>
         </div>
       </div>
