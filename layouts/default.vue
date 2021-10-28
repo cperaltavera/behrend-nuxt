5722640e5d984556d15624a16c5615813356a843
laloinsane <calisto.michel@gmail.com>
2021-10-28 14:53:34 -0300

    update floating button more vert

--- layouts/default.vue
+++ layouts/default.vue
@@ -104,15 +104,14 @@
       </div>
     </footer>
     <div class="fixed-action-btn">
-        <a class="btn-floating btn-large red">
-            <i class="large material-icons">mode_edit</i>
-        </a>
-        <ul>
-            <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-            <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-            <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-            <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
-        </ul>
+      <a class="btn-floating btn-large teal">
+        <i class="large material-icons">more_vert</i>
+      </a>
+      <ul>
+        <li><a class="btn-floating red"><i class="material-icons">email</i></a></li>
+        <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+        <li><a class="btn-floating blue"><i class="material-icons">phone</i></a></li>
+      </ul>
     </div>
   </div>
 </template>
